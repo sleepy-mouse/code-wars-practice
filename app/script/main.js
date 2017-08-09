582556eeffa57f29e2fce1169a6511dd0a47057d
Chris Qiu <chrisqiu0905@gmail.com>
2017-08-09 18:20:54 +0930

    [feature] - add Shortest Word implementation
    https://www.codewars.com/kata/shortest-word/javascript

--- app/script/main.js
+++ app/script/main.js
@@ -1,16 +1,28 @@
 const info = console.info;
 {
-    // Array.diff
-    // https://www.codewars.com/kata/array-dot-diff/javascript
+    /**
+     * Array.diff
+     * https://www.codewars.com/kata/array-dot-diff/javascript
+     * @param a
+     * @param b
+     * @returns {Array}
+     */
     function array_diff(a, b) {
         return a && b ? a.filter(e => !b.includes(e)) : [];
     }
 
-    // info('array_diff([1, 2], [1]) === [2]: %o', _.isEqual(array_diff([1, 2], [1]), [2]));
-    // info('array_diff([1, 2, 2, 2, 3], [2]) === [1, 3]: %o', _.isEqual(array_diff([1, 2, 2, 2, 3], [2]), [1, 3]));
-    // info('array_diff([1, 2, 2], [1]) === [2, 2]: %o', _.isEqual(array_diff([1, 2, 2], [1]), [2, 2]));
-    // info('array_diff([], [1, 2]) === []: %o', _.isEqual(array_diff([], [1, 2]), []));
-    // info('array_diff([1, 2, 2], []) === [1, 2, 2]: %o', _.isEqual(array_diff([1, 2, 2], []), [1, 2, 2]));
     module.exports.array_diff = array_diff;
 }
+{
+    /**
+     * Shortest Word
+     * https://www.codewars.com/kata/shortest-word/javascript
+     * @param s
+     * @returns {number}
+     */
+    function findShort(s) {
+        return Math.min(...s.split(/\s+/).map(e => e.length));
+    }
 
+    module.exports.findShort = findShort;
+}
--- app/script/main.spec.js
+++ app/script/main.spec.js
@@ -10,4 +10,12 @@
             assert.deepEqual(array_diff([1, 2, 2], []), [1, 2, 2]);
         });
     });
+
+    const findShort = require('./main.js').findShort;
+    describe('findShort', function () {
+        it('findShort', function () {
+            assert.equal(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+            assert.equal(findShort("turns out random test cases are easier than writing out basic ones"), 3);
+        });
+    });
 }
